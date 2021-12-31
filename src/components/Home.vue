04b3c0a1653b2f3af7c8168f6678669b4a56481f
oajh04 <cloudtubekr@naver.com>
2021-12-31 20:06:39 +0900

    ⚡️ :: 홈 수정

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,52 +1,112 @@
 <template>
     <div>
-        Home
-        <div>
-            <div v-if="loading"> loading </div>
-            <div v-else>
-                <div v-for="b in boards" v-bind:key="b.id">
-                    {{b}}
-                </div>
-            </div>
-
-
-            <ul>
-                <li>
-                    <router-link to="/b/1">board-1</router-link>
-                </li>
-                <li>
-                    <router-link to="/b/2">board-2</router-link>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <div class="home-title">Personal Boards</div>
+    <div class="board-list" ref="boardList">
+      <div class="board-item" v-for="b in boards" :key="b.id" 
+        :data-bgcolor="b.bgColor" ref="boardItem">
+        <router-link :to="`/b/${b.id}`">
+          <div class="board-item-title">{{b.title}}</div>
+        </router-link>
+      </div>
+      <div class="board-item board-item-new">
+        <a class="new-board-btn" href="" @click.prevent="addBoard">
+          Create new board...
+        </a>
+       </div>
+     </div>
+     <AddBoard v-if="isAddBoard" @close="isAddBoard=false" @submit="onAddBoard"/>
+   </div>
 </template>
 
 <script>
 import { board } from '../api'
+import AddBoard from './AddBoard.vue'
 
 export default {
-    data() {
-        return {
-            loading: false,
-            boards: [],
-        }
-    },
-    created(){
-        this.fetchData()
-    },
-    methods: {
-        fetchData() {
-            this.loading = true
-            board.fetch()
-                .then((res) => this.boards = res)
-                .finally(() => this.loadin = false)
-        }
+  components: {AddBoard},
+  data() {
+    return {
+      loading: false,
+      boards: [],
+      error: '',
+      isAddBoard: false
     }
+   },
+   created() {
+     this.fetchData()
+   },
+  updated() {
+    this.$refs.boardItem.forEach(el => {
+      el.style.backgroundColor = el.dataset.bgcolor
+    })
+  },
+   methods: {
+     fetchData() {
+       this.loading = true
+       board.fetch()
+         .then(data => {
+          this.boards = data.list
+         })
+         .finally(_=> {
+           this.loading = false
+         })
+    },
+    onAddBoard(title) {
+      board.create(title)
+      .then(() => this.fetchData())
+    },
+    addBoard() {
+      this.isAddBoard = true
+     }
+   }
 
 }
 </script>
 
 <style>
-
-</style>
+.home-title {
+  padding: 10px;
+  font-size: 18px;
+  font-weight: bold;
+}
+.board-list {
+  padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.board-item {
+  width: 23%;
+  height: 100px;
+  margin: 0 2% 20px 0;
+  border-radius: 3px;
+}
+.board-item-new {
+  background-color: #ddd;
+}
+.board-item a {
+  text-decoration: none;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.board-item a:hover,
+.board-item a:focus {
+  background-color: rgba(0,0,0, .1);
+  color: #666;
+}
+.board-item-title {
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  padding: 10px;
+}
+.board-item a.new-board-btn {
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+  height: 100px;
+  width: inherit;
+  color: #888;
+  font-weight: 700;
+}
+ </style>
