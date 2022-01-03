657ce42ed7a90468454aabb46c691d9819452a95
oajh04 <cloudtubekr@naver.com>
2022-01-03 14:26:16 +0900

    ⚡️ :: 변이 추가

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        isAddBoard: false
+    },
+    mutations: {
+        SET_IS_ADD_BOARD (state, toggle) {
+            state.isAddBoard = toggle
+        }
+    }
+})
+
+export default store;
