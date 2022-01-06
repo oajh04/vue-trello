50d5cfac26bbf506cbe8c286dfc380c41915442a
oajh04 <cloudtubekr@naver.com>
2022-01-07 02:02:28 +0900

    ⚡️ :: 카드 생성 및 카드 리스트

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,12 +3,37 @@
     <div class="list-header">
       <div class="list-header-title">{{data.title}}</div>
     </div>
+
+    <div class="card-list">
+      <CardItem v-for="card in data.cards" :key="card.id" :data="card"/>
+    </div>
+
+    <div v-if="isAddCard">
+      <AddCard @close="isAddCard=false" :listId="data.id"/>
+    </div>
+    <div v-else>
+      <a class="add-card-btn" href="" @click.prevent="isAddCard=true">
+        &plus; Add a Card...
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
+import AddCard from './AddCard.vue'
+import CardItem from './CardItem.vue'
+
 export default {
+  components: {
+    AddCard,
+    CardItem
+  },
   props: ['data'],
+  data() {
+    return {
+      isAddCard: false
+    }
+  }
 }
 </script>
 
